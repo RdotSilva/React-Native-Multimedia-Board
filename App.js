1793e19172daf69bde0dff9df4c3b1b146ca8d97
Ryan Silva <RyandotSilva@gmail.com>
2019-05-19 14:18:15 -0400

    Add Expo Video component

--- App.js
+++ App.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Cat Sounds</Text>
+				<Expo.Video
+					source={require('./assets/1.mp4')}
+					style={{
+						width: 400,
+						height: 400
+					}}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
